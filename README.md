6b3ca328ee395ea3a6e438788717283a1c6781d8
Taylor Blau <me@ttaylorr.com>
2017-07-25 09:32:58 -0600

    all: initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# verify-pack(1)
+
+`verify-pack(1)` parses v1 and v2 packfiles and prints the locations of the
+objects contained within them.
+
+## Usage
+
+`verify-pack(1)` accepts the filepath of a v1 or v2 packfile as its argument,
+and a line-delimited list of object names (in ASCII-encoded SHA-1 format) on
+stdin.
+
+## License
+
+MIT.
---
+++ cmd/verify-pack/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	"github.com/git-lfs/git-lfs/git/odb/pack"
+	"github.com/pkg/errors"
+
+	"golang.org/x/exp/mmap"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		die(usage())
+	}
+
+	f, err := mmap.Open(os.Args[1])
+	if err != nil {
+		die(errors.Wrap(err, "could not open index file").Error())
+	}
+
+	idx, err := pack.DecodeIndex(f)
+	if err != nil {
+		die(errors.Wrap(err, "could not decode index").Error())
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		name := scanner.Text()
+
+		oid, err := hex.DecodeString(name)
+		if err != nil {
+			die(errors.Wrapf(err, "could not decode name %q", name).Error())
+		}
+
+		e, err := idx.Entry(oid)
+		if err != nil {
+			die(errors.Wrapf(err, "could not find entry %q", name).Error())
+		}
+
+		fmt.Printf("%+v\n", e)
+	}
+
+	if err := scanner.Err(); err != nil {
+		die(err.Error())
+	}
+
+	if err := f.Close(); err != nil {
+		die(errors.Wrap(err, "could not close file").Error())
+	}
+}
+
+func die(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+	os.Exit(1)
+}
+
+func usage() string {
+	return fmt.Sprintf("usage: %s <pack>", os.Args[0])
+}
